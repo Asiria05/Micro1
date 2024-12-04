4c58094ff03e3aecba90170b56f2d7f3ee6ddf45
Asiria05 <mapachesfuriosos0@gmail.com>
2024-12-03 20:52:02 -0600

    m

--- src/main/java/mx/petcare/mascotas/petcareAPI/repository/ImagesRepository.java
+++
@@ -1,9 +0,0 @@
-package mx.petcare.mascotas.petcareAPI.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import mx.petcare.mascotas.petcareAPI.model.ImagesResource;
-
-public interface ImagesRepository extends MongoRepository<ImagesResource, String> {
-}
-
-
